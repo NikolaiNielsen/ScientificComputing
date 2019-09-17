c1923936928ed7fa5b721c2901ece52ef616ffb9
Nikolai Nielsen <nikolainielsen@outlook.dk>
2019-09-17 13:34:05 +0200

    add rayleigh quotient iteration and option for rayleigh quotient in power iteration

--- project2/functions.py
+++ project2/functions.py
@@ -1,19 +1,26 @@
 import numpy as np
+import sys
+sys.path.append("../project1/")
+import functions1 as p1
 
 
-def power_iteration(A, max_iter=20, normalized=True):
+def power_iteration(A, max_iter=25, rayleigh=False):
     """
     Performs power iteration on the matrix A to find the extreme eigenvalue.
-    Normalized by default.
+    Normalized. With option of using rayleigh quotient.
     """
     n, _ = A.shape
     x = np.random.uniform(size=n)
     for i in range(max_iter):
         y = A@x
         x = y / np.amax(y)
-    else:
+
+    if not rayleigh:
         y = A@x
         return np.amax(y)
+    else:
+        res = rayleigh_quotient(A, x)
+        return res
 
 
 def gershgorin_disks(A):
@@ -23,3 +30,28 @@ def gershgorin_disks(A):
     centers = np.diag(A)
     row_sums = np.sum(np.abs(A), axis=1)-np.abs(centers)
     return centers, row_sums
+
+
+def rayleigh_quotient(A, x):
+    """
+    Computes the rayleigh quotient for a matrix and vector
+    """
+    num = x.dot(A@x)
+    den = np.sum(x**2)
+    lambda_ = num/den
+    return lambda_
+
+
+def rayleigh_quotient_iteration(A, max_iter=10):
+    """
+    Performs Rayleigh Quotient iteration
+    """
+    n, _ = A.shape
+    x = np.random.uniform(size=n)
+
+    for i in range(max_iter-1):
+        sigma = rayleigh_quotient(A, x)
+        B = A-sigma*np.eye(n)
+        y = p1.linsolve(B, x)
+        x = y/np.amax(y)
+    return rayleigh_quotient(A, x)
