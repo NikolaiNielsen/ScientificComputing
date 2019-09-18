46c56c0bcef9754b93f6b20a4025eac8d9564c98
Nikolai Nielsen <nikolainielsen@outlook.dk>
2019-09-18 14:34:17 +0200

    move stuff around. Add shift and convergence

--- f2.py
+++ f2.py
@@ -2,12 +2,34 @@ import numpy as np
 import f1
 
 
-def power_iterate(A, x0=None, max_iter=25, epsilon=1e-6, rayleigh=False):
+def gershgorin(A):
+    """
+    Calculates the Greshgorin discs for a given matrix
+    """
+    centers = np.diag(A)
+    radii = np.sum(np.abs(A), axis=1)-np.abs(centers)
+    return centers, radii
+
+
+def rayleigh_qt(A, x):
+    """
+    Computes the rayleigh quotient for a matrix and vector
+    """
+    num = x.dot(A@x)
+    den = np.sum(x**2)
+    lambda_ = num/den
+    return lambda_
+
+
+def power_iterate(A, x0=None, max_iter=25, epsilon=1e-6, shift=0.):
     """
     Performs power iteration on the matrix A to find the extreme eigenvector.
     Normalized. With option of using rayleigh quotient.
     """
     n, _ = A.shape
+
+    # Set shift, if needed
+    A = A - np.eye(n)*shift
 
     # Choose random x0 if not given one
     x = np.random.uniform(size=n) if x0 is None else x0
@@ -28,35 +50,27 @@ def power_iterate(A, x0=None, max_iter=25, epsilon=1e-6, rayleigh=False):
     return x, i+1
 
 
-def gershgorin(A):
-    """
-    Calculates the Greshgorin discs for a given matrix
-    """
-    centers = np.diag(A)
-    radii = np.sum(np.abs(A), axis=1)-np.abs(centers)
-    return centers, radii
-
-
-def rayleigh_qt(A, x):
-    """
-    Computes the rayleigh quotient for a matrix and vector
-    """
-    num = x.dot(A@x)
-    den = np.sum(x**2)
-    lambda_ = num/den
-    return lambda_
-
-
-def rayleigh_iterate(A, max_iter=10):
+def rayleigh_iterate(A, x0=None, shift=0., epsilon=1e-6, max_iter=10):
     """
     Performs Rayleigh Quotient iteration
     """
     n, _ = A.shape
-    x = np.random.uniform(size=n)
+    A = A - np.eye(n)*shift
+    x = np.random.uniform(size=n) if x0 is None else x0
 
-    for i in range(max_iter-1):
-        sigma = rayleigh_qt(A, x)
+    # Calc approx eigenvalue and shift matrix
+    sigma = rayleigh_qt(A, x)
+    for i in range(max_iter):
         B = A-sigma*np.eye(n)
+
+        # Find new eigenvector
         y = f1.linsolve(B, x)
         x = y/np.amax(y)
-    return rayleigh_qt(A, x)
+
+        # Test for convergence
+        sigma_new = rayleigh_qt(A, x)
+        res = abs((sigma_new-sigma)/(sigma))
+        if res <= epsilon:
+            break
+        sigma = sigma_new
+    return x, i+1
