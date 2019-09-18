def39499e942a460ae727d8897880b0469117431
Nikolai Nielsen <nikolainielsen@outlook.dk>
2019-09-18 15:43:25 +0200

    solve a, b, c

--- p2.py
+++ p2.py
@@ -1,14 +1,62 @@
 import numpy as np
 from f2 import *
+from project2.examplematrices import *
+K = np.load('project2/Chladni-Kmat.npy')
+mats = [A1, A2, A3, A4, A5, A6]
+eigenvals = [eigvals1, eigvals2, eigvals3, eigvals4, eigvals5, eigvals6]
 
-A = np.array([[4, 0.5, 0], [0.6, 5, 0.6], [0, 0.5, 3]])
-centers, radii = gershgorin(A)
 
-max_eig = power_iterate(A)
-max_eig2 = power_iterate(A, rayleigh=True)
-print(max_eig)
-print(max_eig2)
+def a():
+    print("a2: Centers and radii of matrix K")
+    centers, radii = gershgorin(K)
+    for c, r in zip(centers, radii):
+        print(c, r)
+    print()
 
-max_eig = rayleigh_iterate(A)
-print(max_eig)
-# print(np.linalg.eigvals(A)[0])
+
+def b():
+    print("b3: Eigenvectors of example matrices")
+    for n, (A, eigs) in enumerate(zip(mats, eigenvals)):
+        x, k = power_iterate(A)
+        approx = rayleigh_qt(A, x)
+        res = np.sqrt(np.sum((A@x - approx*x)**2))
+        print(f'A{n} converged after {k} iterations')
+        # print(f'Eigenvector: {x}')
+        print(f"Eigenvalue: {approx}")
+        print(f'Residual: {res}')
+        print()
+    print()
+
+    print("b4: largest eigenvalue of K")
+    x, k = power_iterate(K)
+    print(f'eigenvalue: {rayleigh_qt(K, x)}')
+    print()
+
+
+def c():
+    print('c2: Eigenvalues of example matrices')
+    for n, (A, eigs) in enumerate(zip(mats, eigenvals)):
+        x, k = rayleigh_iterate(A)
+        approx = rayleigh_qt(A, x)
+        res = np.sqrt(np.sum((A@x - approx*x)**2))
+        print(f'A{n} converged after {k} iterations')
+        # print(f'Eigenvector: {x}')
+        print(f"Eigenvalue: {approx}")
+        print(f'Residual: {res}')
+        print()
+    print()
+
+
+def d():
+    pass
+
+
+def main():
+    # a()
+    # b()
+    c()
+    d()
+
+
+if __name__ == "__main__":
+    main()
