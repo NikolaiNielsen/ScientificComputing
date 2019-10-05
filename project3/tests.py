da293a5847fad8a134afea5b0171122444b10733
Nikolai Nielsen <nikolainielsen@outlook.dk>
2019-10-05 21:09:38 +0200

    add more tests

--- project3/tests.py
+++ project3/tests.py
@@ -43,9 +43,9 @@ def test_potential():
                   [1, 0, 0],
                   [0, 1, 0.]])
 
-    AnalyticGrad = np.array([[6, 6, 0],
-                             [-183/32, -9/32, 0],
-                             [-9/32, -183/32, 0]])
+    AnalyticGrad = np.array([[-6, -6, 0],
+                             [183/32, 9/32, 0],
+                             [9/32, 183/32, 0]])
 
     AnalyticalPot = -7/64
 
@@ -60,6 +60,20 @@ def test_potential():
 
     # print(f'potential: {pot(r)}')
     print(f'Potential residual:  {pot(r)-AnalyticalPot}')
+
+
+def test_gradient():
+    data = np.genfromtxt('Ar-lines.csv', delimiter=' ')
+    grad = gradient_total(data)
+    s = -grad
+    x = data + s
+    c = ['b']*20 + ['r']*20
+    fig, ax = plt.subplots(subplot_kw=dict(projection='3d'))
+    ax.scatter(data.T[0], data.T[1], data.T[2], c=c)
+    ax.quiver(data.T[0], data.T[1], data.T[2],
+              s.T[0], s.T[1], s.T[2], length=0.02)
+    ax.scatter(x.T[0], x.T[1], x.T[2], c=c)
+    plt.show()
 
 
 def test_gss():
@@ -78,19 +92,52 @@ def test_gss():
 def show_first_iterations():
     data = np.genfromtxt('Ar-lines.csv', delimiter=' ')
     alpha_max = 1
-    x = conjugate_gradient(potential_total, data, g=gradient_total,
-                           alpha_max=alpha_max,
-                           max_iter=9, epsilon=1e-6)
+    x, s = conjugate_gradient(potential_total, data, g=gradient_total,
+                              alpha_max=alpha_max,
+                              max_iter=9, epsilon=1e-6, return_s=True)
 
     fig, ax = plt.subplots(subplot_kw=dict(projection='3d'), nrows=3, ncols=3)
     ax = ax.flatten()
-    for n, (Ax, X) in enumerate(zip(ax, x)):
+    for n, (Ax, X, S) in enumerate(zip(ax, x, s)):
         Xs = X.T
+        ss = S.T
+        grad = -gradient_total(Xs.T).T
         Ax.scatter(Xs[0], Xs[1], Xs[2])
+        Ax.quiver(Xs[0], Xs[1], Xs[2], grad[0], grad[1], grad[2], length=0.01)
         Ax.set_title(f'{n}')
     fig.suptitle(r'First iterations. $\alpha_{max}= $' + str(alpha_max))
     fig.tight_layout()
     return fig, ax
+
+
+def test_second_step():
+    def evaluator(alpha, args):
+        x0, s = args
+        return x0 + alpha*s
+
+    c = ['b']*20 + ['r']*20
+    x0 = np.genfromtxt('Ar-lines.csv', delimiter=' ')
+    s = -gradient_total(x0)
+    alpha = gss(potential_total, 0, 1, [evaluator, x0, s])
+    x1 = x0 + alpha*s
+    s2 = -gradient_total(x1)
+    beta = s2.flatten().dot(s2.flatten()) / s.flatten().dot(s.flatten())
+    s3 = s2 + beta*s
+
+    fig, (ax1, ax2) = plt.subplots(subplot_kw=dict(projection='3d'), ncols=2)
+    ax1.scatter(x1.T[0], x1.T[1], x1.T[2], c=c)
+    ax1.quiver(x1.T[0], x1.T[1], x1.T[2], s2.T[0], s2.T[1], s2.T[2],
+               length=0.01)
+
+    alpha = gss(potential_total, 0, 1, [evaluator, x1, s2])
+    print(alpha)
+    x2 = x1 + alpha*s2
+    s3 = -gradient_total(x2)
+    ax2.scatter(x2.T[0], x2.T[1], x2.T[2], c=c)
+    ax2.quiver(x2.T[0], x2.T[1], x2.T[2], s3.T[0], s3.T[1], s3.T[2],
+               length=0.01)
+
+    plt.show()
 
 
 def show_line_search(Norm=True):
@@ -134,5 +181,5 @@ def main():
 
 
 if __name__ == "__main__":
-    fig, ax = show_first_iterations()
+    fig2, ax2 = show_first_iterations()
     plt.show()
