a66a1af82f66bb530e272af5ba7875e47c0d924c
Nikolai Nielsen <nikolainielsen@outlook.dk>
2019-10-05 21:10:12 +0200

    add 1D potential, revamp q1 and q2, do more tests

--- project3/p3.py
+++ project3/p3.py
@@ -6,6 +6,7 @@ from scipy.spatial.distance import pdist, cdist
 from scipy.optimize import fmin_cg, line_search
 from f3 import *
 from progress.bar import Bar
+import timeit
 
 
 np.seterr(all='raise')
@@ -14,6 +15,11 @@ EPSILON = 0.997
 SIGMA = 3.401
 A = 4*EPSILON*SIGMA**12
 B = 4*EPSILON*SIGMA**6
+
+
+def potential1D(r, r0=0):
+    V = A/(r-r0)**12 - B/(r-r0)**6
+    return V
 
 
 def potential(R2, A=A, B=B):
@@ -38,9 +44,9 @@ def potential_total(r, A=A, B=B):
 
 
 def gradient_one_atom(r_other, x0, A=A, B=B):
-    R2 = cdist(r_other, x0.reshape((1, 3)), metric='sqeuclidean')
-    frac = - 12*A/R2**7 + 6*B/R2**4
-    coord_dists = x0 - r_other
+    R2 = cdist(r_other, x0.reshape((1, 3)), metric='euclidean')
+    frac = - 12*A/R2**14 + 6*B/R2**8
+    coord_dists = r_other - x0
     grad = np.sum(coord_dists*frac, axis=0)
     return grad
 
@@ -54,6 +60,18 @@ def gradient_total(r, A=A, B=B, normalize=True):
     if normalize:
         grad = grad/abs(np.max(grad))
     return grad
+
+
+def gradient_total2(r, A=A, B=B, normalize=True):
+    r = r.reshape((-1, 3))
+    grad = np.zeros(r.shape)
+    for n, atom in enumerate(r):
+        r_other = r[np.arange(r.shape[0]) != n]
+        grad[n] = gradient_one_atom(r_other, atom, A, B)
+
+    if normalize:
+        grad = grad/abs(np.max(grad))
+    return grad.flatten()
 
 
 def get_gradient(f, r, h=1e-4, normalize=True):
@@ -89,62 +107,100 @@ def get_gradient(f, r, h=1e-4, normalize=True):
     return grad
 
 
-
-
 def q1():
     r1 = np.linspace(1.65, 10.0, num=500)
     r0 = 0
-    p1 = potential(r1, r0)
+    p1 = potential1D(r1, r0)
     fig, (ax, ax2) = plt.subplots(nrows=2)
-    ax.plot(r1, p1)
-    ax.plot(r1[p1 < 0], p1[p1 < 0])
-    r2 = np.linspace(2, 4.5, 100)
-    p2 = potential(r2, r0)
-    ax2.plot(r2, p2)
-    # ax2.plot(r2[p2 < 0], p2[p2 < 0])
-    ax2.set_ylim(-1.5, 7.5)
+    ax.plot(r1, p1, label='V > 0')
+    ax.plot(r1[p1 < 0], p1[p1 < 0], label='V < 0')
+    r2 = np.linspace(3, 5, 100)
+    p2 = potential1D(r2, r0)
+    ax2.plot(r2, p2, label='V > 0')
+    ax2.plot(r2[p2 < 0], p2[p2 < 0], label='V < 0')
+    ax.set_xlabel('Distance $r$')
+    ax.set_ylabel('Potential $V$')
+    ax2.set_xlabel('Distance $r$')
+    ax2.set_ylabel('Potential $V$')
+    ax.legend()
+    ax2.legend()
+    ax.set_title('12-6 Lennard-Jones potential between two atoms')
+    fig.tight_layout()
 
-    x = newton_raphson(potential, x0=2)
-    fx = potential(x)
+    setup = 'from p3 import potential_total\nimport numpy as np\n'
+    setup += "data = np.genfromtxt('Ar-lines.csv', delimiter=' ')"
+    N = 1000
+    exec_time = timeit.timeit("potential_total(data)", setup=setup, number=N)/N
+    print(f"Avg calculation time of total potential: {exec_time:.4e} s")
+    return fig, ax, ax2
 
-    x2 = bisection(potential, 2, 4)
-    x3 = secant(potential, 1.65, 2)[-1]
-    x4 = inverse_quadratic(potential, 1, 2, 3)[-1]
-    print(x[-1])
-    print(x2)
-    print(x3)
-    print(x4)
-    ax2.plot(x, fx, '-o')
-    plt.show()
+
+def q2():
+    x1 = bisection(potential1D, 2, 4)
+    x2 = newton_raphson(potential1D, x0=2)[-1]
+    x3 = secant(potential1D, 1.65, 2)[-1]
+    x4 = inverse_quadratic(potential1D, 1, 2, 3)[-1]
+
+    s = "from f3 import newton_raphson, bisection, secant, inverse_quadratic"
+    s = s + "\nfrom p3 import potential1D"
+    N = 1000
+    bisection_time = timeit.timeit("bisection(potential1D, 2, 4)",
+                                   setup=s, number=N)/N
+    newton_raphson_time = timeit.timeit("newton_raphson(potential1D, x0=2)",
+                                        setup=s, number=N)/N
+    secant_time = timeit.timeit("secant(potential1D, 1.65, 2)",
+                                setup=s, number=N)/N
+    quadratic_time = timeit.timeit("inverse_quadratic(potential1D, 1, 2, 3)",
+                                   setup=s, number=N)/N
+
+    print("Roots of 12-6 Lennard-Jones potential found:\n")
+    print("Bisection Method:")
+    print(f"Root: {x1:.3f}")
+    print(f"Avg time: {bisection_time:.3e}\n")
+
+    print("Newton-Raphson Method with numerical derivative:")
+    print(f"Root: {x2:.3f}")
+    print(f"Avg time: {newton_raphson_time:.3e}\n")
+
+    print("Secant Method:")
+    print(f"Root: {x3:.3f}")
+    print(f"Avg time: {secant_time:.3e}\n")
+
+    print("Inverse Quadratic Method:")
+    print(f"Root: {x4:.3f}")
+    print(f"Avg time: {quadratic_time:.3e}")
 
 
 def q3():
     data = np.genfromtxt('Ar-lines.csv', delimiter=' ')
-    alpha_max = 1
+    alpha_max = 1e1
     x = conjugate_gradient(potential_total, data, g=gradient_total,
+                           g2=gradient_total2,
                            alpha_max=alpha_max,
-                           max_iter=200, epsilon=1e-6)
-    # N = len(x)
+                           max_iter=2000, epsilon=1e-9)
+
+    c = ['b']*20 + ['r']*20
+    # for n in range(len(x)-1):
+    #     res = x[n+1] - x[n]
+    #     print(np.sum(np.sqrt(np.sum(res**2, axis=1))))
     fig, ax = plt.subplots(subplot_kw=dict(projection='3d'))
 
-    # writer = anim.FFMpegWriter(fps=60)
+    # writer = anim.FFMpegWriter(fps=1)
     # bar = Bar('Writing movie', max=len(x))
-
     # dpi = 200
     # outfile = 'movie.mp4'
     # with writer.saving(fig, outfile, dpi):
     #     for X in x:
     #         X = X.T
-    #         ax.scatter(X[0], X[1], X[2])
+    #         ax.scatter(X[0], X[1], X[2], c=c)
     #         writer.grab_frame()
     #         ax.clear()
     #         bar.next()
     # bar.finish()
-    x = x[-1].T
-    grad = gradient_total(x.T, normalize=False)
-    print(grad)
 
-    ax.scatter(x[0], x[1], x[2])
+    last = x[-1].T
+    ax.scatter(last[0], last[1], last[2])
+    # ax.quiver(last[0], last[1], last[2], s[0], s[1], s[2], length=1)
     plt.show()
 
 
@@ -154,11 +210,12 @@ def scipy_solution():
     # result, alliters = fmin_cg(potential_total, data.flatten(), retall=True)
     # np.save('alliter', alliters)
     alliters = alliters.reshape((-1, 40, 3))
-    first = alliters[30].T
+    first = alliters[1].T
 
     # x = result.reshape((-1, 3)).T
     fig, ax = plt.subplots(subplot_kw=dict(projection='3d'))
-    ax.scatter(first[0], first[1], first[2])
+    c = ['b']*20 + ['r']*20
+    ax.scatter(first[0], first[1], first[2], c=c)
     plt.show()
 
 
@@ -167,4 +224,4 @@ def main():
 
 
 if __name__ == "__main__":
-    scipy_solution()
+    main()
