de35ee64715eca0fcc3f0d41c1362717a1f06e7d
Nikolai Nielsen <nikolainielsen@outlook.dk>
2019-11-04 11:47:30 +0100

    implicit method works now

--- project4/make_matrix.py
+++ project4/make_matrix.py
@@ -40,14 +40,14 @@ def create_diffusion_jacobian(p, c, dt, h, sparse=True):
     else:
         jac = np.zeros(((N)**2, (N)**2))
     # Populating inner part of the jacobian
-    const1 = 1-2*dt*c/h**2
-    const2 = -dt*c/(2*h**2)
+    const1 = 1+4*dt*c/h**2
+    const2 = -dt*c/(h**2)
     for i in range(1, N-1):
         # sides
-        left = indexHelper(np.array([i, i]), np.array([0, 1]), N)
-        right = indexHelper(np.array([i, i]), np.array([N-1, N-2]), N)
-        jac[[left[0], left[0]], left] = [1, -1]
-        jac[[right[0], right[0]], right] = [1, -1]
+        left = indexHelper(i, 0, N)
+        right = indexHelper(i, N-1, N)
+        jac[left, left] = 1
+        jac[right, right] = 1
 
         # inner
         for j in range(1, N-1):
@@ -57,10 +57,10 @@ def create_diffusion_jacobian(p, c, dt, h, sparse=True):
             jac[[m, m, m, m, m], ij] = [const1, const2, const2, const2, const2]
     for i in range(0, N):
         # Upper and lower
-        ij = indexHelper(np.array([0, 1]), np.array([i, i]), N)
-        ij2 = indexHelper(np.array([N-1, N-2]), np.array([i, i]), N)
-        jac[[i, i], ij] = [1, -1]
-        jac[[ij2[0], ij2[0]], ij2] = [1, -1]
+        ij = indexHelper(0, i, N)
+        ij2 = indexHelper(N-1, i, N)
+        jac[ij, ij] = 1
+        jac[ij2, ij2] = 1
     return jac
 
 
@@ -79,26 +79,38 @@ def test_2D():
     xx, yy = np.meshgrid(x, x)
     p = np.zeros((Nx+2, Nx+2))
     c = 1
-    dt = (h**2/(2*c))**2
-    Nt = 200
+    dt = h**2/(3*c)
+    Nt = 100
     jac = create_diffusion_jacobian(p, c, dt, h, sparse=True)
     # print(jac)
     jac = jac.tocsr()
-    # sigma = 0.1
-    # p = np.exp(-((xx-0.5)**2/(2*sigma**2) + (yy-0.5)**2/(2*sigma**2)))
-    p[Nx//2, Nx//2] = 10
+    sigma = 0.1
+    p = np.exp(-((xx-0.5)**2/(2*sigma**2) + (yy-0.5)**2/(2*sigma**2)))
+    p[0] = 0
+    p[-1] = 0
+    p[:, 0] = 0
+    p[:, -1] = 0
+    # p0 = p.copy()
+    # p[Nx//2, Nx//2] = 10
     pold = p.copy()
 
     xx, yy = np.meshgrid(x, x)
-    # for i in range(1, Nt):
-    b = create_b_vec(p, c, h, dt)
-    p = splin.spsolve(jac, b).reshape((Nx+2, Nx+2))
+    for i in range(1, Nt):
+        # b = create_b_vec(p, c, h, dt)
+        implicit = splin.spsolve(jac, p.flatten()).reshape((Nx+2, Nx+2))
+        # explicit = create_b_vec(p, c, h, dt).reshape((Nx+2, Nx+2))
+        p = (implicit) # + explicit)/2
+        p[0] = 0
+        p[-1] = 0
+        p[:, 0] = 0
+        p[:, -1] = 0
 
     # fig, ax = plt.subplots(subplot_kw=dict(projection='3d'))
     # ax.plot_surface(xx, yy, p)
     fig, ax = plt.subplots()
     ax.plot(p[50])
     print(p[50, 50])
+    ax.plot(pold[50])
     plt.show()
 
 
@@ -131,7 +143,7 @@ def test_1D():
     p = 1/np.sqrt(2*np.pi*(0.25**2)) * np.exp(-(x-0.5)**2/(2*0.25**2))
     P = [p.copy()]
     for i in range(1, Nt):
-        p = np.linalg.solve(A, B@p)
+        p = np.linalg.solve(A, p)
         P.append(p.copy())
 
     p = np.array(P)
@@ -142,6 +154,21 @@ def test_1D():
     ax.plot_surface(tt, xx, p)
 
     plt.show()
+
+
+def test_mat():
+    Nx = 3
+    x, h = linspace_with_ghosts(0, 1, Nx)
+    xx, yy = np.meshgrid(x, x)
+    p = np.zeros((Nx+2, Nx+2))
+    c = 1
+    dt = 1
+    jac = create_diffusion_jacobian(p, c, dt, h)
+    print(jac.toarray())
+    p[2, 2] = 10
+    sol = splin.spsolve(jac, p.flatten()).reshape((5, 5))
+    print(sol)
+    # fig, ax = plt.subplots()
 
 
 def main():
