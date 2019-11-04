a40e2d445b7341237f33ea3c15bc01de2eca4a4e
Nikolai Nielsen <nikolainielsen@outlook.dk>
2019-11-04 15:37:43 +0100

    expand indexHelper. Add jacobian and b-vector for coupled diffusion

--- project4/make_matrix.py
+++ project4/make_matrix.py
@@ -23,11 +23,12 @@ def calc_laplace(z, dx):
     return z_lap
 
 
-def indexHelper(i, j, N):
+def indexHelper(i, j, N, n=0):
     """
-    Convert matrix-indexing (i,j) to vector-indexing (m). Assumes a NxN matrix.
+    Convert matrix-indexing (i,j) to vector-indexing (m). Assumes a
+    (n+1)N x n(+1)N matrix.
     """
-    return N * i + j
+    return N**2 * n + N * i + j
 
 
 def create_implicit_diffusion_jacobian(p, c, dt, h, sparse=True):
@@ -104,7 +105,91 @@ def CN_diffusion_jacobian(p, c, dt, h, sparse=True):
     return jac
 
 
-def CN_b_vec(p, c, h, dt):
+def CN_coupled_diffusion_jac(p, params, h, dt):
+    """
+    Creates the jacobian for the coupled diffusion problem.
+    """
+    n = p.shape[0]
+    Dp, Dq, cp, cq = params
+    jac = sp.lil_matrix((2*n*n, 2*n*n))
+    N = jac.shape[0]
+
+    p_const1 = 1+2*dt*Dp/h**2
+    p_const2 = -dt*Dp/(2*h**2)
+    p_const3 = -cp*dt/2
+
+    q_const1 = 1+2*dt*Dq/h**2
+    q_const2 = -dt*Dq/(2*h**2)
+    q_const3 = -cq*dt/2
+
+    for i in range(1, n-1):
+        # top ghosts:
+        ptop = indexHelper(0, i, n, 0)
+        qtop = indexHelper(0, i, n, 1)
+        jac[ptop, ptop] = 1
+        jac[qtop, qtop] = 1
+
+        # Bottom ghosts
+        pbot = indexHelper(n-1, i, n, 0)
+        qbot = indexHelper(n-1, i, n, 1)
+        jac[pbot, pbot] = 1
+        jac[qbot, qbot] = 1
+
+        for j in range(1, n-1):
+            # Populate inner nodes:
+
+            # The i's and j's needed for the laplacian
+            i_arr = np.array([i, i+1, i-1, i, i])
+            j_arr = np.array([j, j, j, j+1, j-1])
+            # Converted to vector index
+            pij = indexHelper(i_arr, j_arr, n, 0)
+            qij = indexHelper(i_arr, j_arr, n, 1)
+
+            # The current node for p and q
+            pm = pij[0]
+            qm = qij[0]
+
+            # Laplacian
+            jac[[pm]*5, pij] = [p_const1] + [p_const2] * 4
+            jac[[qm]*5, qij] = [q_const1] + [q_const2] * 4
+
+            # Coupling between p and q:
+            jac[pm, qm] = p_const3
+            jac[qm, pm] = q_const3
+
+    for i in range(0, n):
+        # left and right nodes.
+        pleft = indexHelper(i, 0, n, 0)
+        qleft = indexHelper(i, 0, n, 1)
+        pright = indexHelper(i, n-1, n, 0)
+        qright = indexHelper(i, n-1, n, 1)
+        jac[pleft, pleft] = 1
+        jac[qleft, qleft] = 1
+        jac[pright, pright] = 1
+        jac[qright, qright] = 1
+
+    return jac
+
+
+def coupled_diffusion_b_vec(p, q, params, h, dt):
+    """
+    Calculates the b-vector for the coupled diffusion problem.
+    """
+    Dp, Dq, cp, cq = params
+
+    pvec = p.copy()
+    plap = calc_laplace(p, h)
+    pvec[1:-1, 1:-1] = p[1:-1, 1:-1] + plap * Dp * dt / 2 + cp * q[1:-1, 1:-1]
+
+    qvec = q.copy()
+    qlap = calc_laplace(q, h)
+    qvec[1:-1, 1:-1] = q[1:-1, 1:-1] + qlap * Dq * dt / 2 + cq * p[1:-1, 1:-1]
+
+    b = np.array((pvec, qvec)).flatten()
+    return b
+
+
+def diffusion_b_vec(p, c, h, dt):
     b = np.zeros(p.shape)
     N = p.shape[0]-2
     lap = calc_laplace(p, h)
@@ -157,6 +242,7 @@ def test_implicit_diffusion():
 def test_CN_diffusion():
     # Initialize grid
     Nx = 101
+    Nxp = Nx + 2
     x, h = linspace_with_ghosts(0, 1, Nx)
     xx, yy = np.meshgrid(x, x)
 
@@ -164,7 +250,7 @@ def test_CN_diffusion():
     sigma = 0.1
     # p = np.exp(-((xx-0.5)**2/(2*sigma**2) + (yy-0.5)**2/(2*sigma**2)))
     p = np.zeros(xx.shape)
-    p[Nx//2, Nx//2] = 10
+    p[Nxp//2, Nxp//2] = 10
     p[0] = 0
     p[-1] = 0
     p[:, 0] = 0
@@ -173,21 +259,26 @@ def test_CN_diffusion():
     pold = p.copy()
     # Set parameters
     c = 1
-    dt = h**2/(3*c)
+    dt = h**2/(2*c)
     Nt = 100
     jac = create_implicit_diffusion_jacobian(p, c, dt, h, sparse=True)
     jac = jac.tocsr()
 
+    p_mid = [p[Nxp//2]]
+
     for i in range(1, Nt):
-        b = CN_b_vec(p, c, h, dt)
+        b = diffusion_b_vec(p, c, h, dt)
         p = splin.spsolve(jac, b).reshape(xx.shape)
         p[0] = 0
         p[-1] = 0
         p[:, 0] = 0
         p[:, -1] = 0
+        p_mid.append(p[Nxp//2])
 
+    t = dt*np.arange(Nt)
+    X, T = np.meshgrid(x, t)
     fig, ax = plt.subplots(subplot_kw=dict(projection='3d'))
-    ax.plot_surface(xx, yy, p)
+    ax.plot_surface(X, T, np.array(p_mid))
     plt.show()
 
 
@@ -233,8 +324,18 @@ def test_1D_CN_diffusion():
     plt.show()
 
 
+def test_matrix():
+    Nx = 6
+    x, h = linspace_with_ghosts(0, 5, Nx)
+    p = np.outer(x, x)
+    J = CN_coupled_diffusion_jac(p, [1, 1, 1, 1], 1, 1)
+    fig, ax = plt.subplots()
+    ax.spy(J)
+    plt.show()
+
+
 def main():
-    test_CN_diffusion()
+    test_matrix()
 
 
 if __name__ == "__main__":
