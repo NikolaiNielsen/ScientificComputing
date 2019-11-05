fbce3f2288a0a061ba575dcd3ba29f401a8ad81e
Nikolai Nielsen <nikolainielsen@outlook.dk>
2019-11-05 12:44:51 +0100

    add comments. CN is still unstable

--- project4/CN_RD.py
+++ project4/CN_RD.py
@@ -34,16 +34,19 @@ def indexHelper(i, j, N, n=0):
 
 def RD_jacobian_const(p, params, h, dt):
     """
-    Creates the jacobian for the Reaction Diffusion problem.
+    Creates the constant parts of the jacobian for the reaction diffusion.
+    Leaves boundary conditions alone - these are handled in "update_ghosts".
     """
     Nx = p.shape[0]
     Dp, Dq, C, K = params
     jac = sp.lil_matrix((2*Nx*Nx, 2*Nx*Nx))
     N = jac.shape[0]
 
+    # Diagonal and off-diagonal
     p_const1 = 1+(K+1+4*Dp/h**2)*dt/2
     p_const2 = -dt*Dp/(2*h**2)
 
+    # Diagonal, off-diagonal, and coupling.
     q_const1 = 1+2*dt*Dq/h**2
     q_const2 = -dt*Dq/(2*h**2)
     q_const3 = -dt*K/2
@@ -64,10 +67,10 @@ def RD_jacobian_const(p, params, h, dt):
         for j in range(1, Nx-1):
             # Populate inner nodes:
 
-            # The i's and j's needed for the laplacian
+            # The i's and j's (in p and q indices) needed for the laplacian
             i_arr = np.array([i, i+1, i-1, i, i])
             j_arr = np.array([j, j, j, j+1, j-1])
-            # Converted to vector index
+            # Converted to jacobian indices
             pij = indexHelper(i_arr, j_arr, Nx, 0)
             qij = indexHelper(i_arr, j_arr, Nx, 1)
 
@@ -75,7 +78,7 @@ def RD_jacobian_const(p, params, h, dt):
             pm = pij[0]
             qm = qij[0]
 
-            # Laplacian
+            # Add the constants on the diagonal and those for the laplacian
             jac[[pm]*5, pij] = [p_const1] + [p_const2] * 4
             jac[[qm]*5, qij] = [q_const1] + [q_const2] * 4
 
@@ -98,22 +101,36 @@ def RD_jacobian_const(p, params, h, dt):
 
 def RD_jacobian_diag(p, q, params, h, dt):
     """
+    Creates a sparse matrix of the non-constant parts of the jacobian.
     """
     N = p.shape[0]
     Dp, Dq, C, K = params
+
+    # The p and q parts
     p_contrib = np.zeros(p.shape)
     p_contrib[1:-1, 1:-1] = -dt * p[1:-1, 1:-1] * q[1:-1, 1:-1]
     q_contrib = np.zeros(q.shape)
     q_contrib[1:-1, 1:-1] = dt/2 * p[1:-1, 1:-1] * p[1:-1, 1:-1]
+
+    # The main diagonal consists of first the p-part and then the q-part
     diag1 = np.array((p_contrib, q_contrib)).flatten()
+
+    # The coupling diagonals
     diag2 = p_contrib.flatten()
     diag3 = q_contrib.flatten()
+
+    # We offset the first diagonal by 0, and the others by the total number of
+    # nodes, so pij maps to qij.
     offsets = [0, -N*N, N*N]
     jac = sp.diags([diag1, diag2, diag3], offsets)
     return jac
 
 
 def f_consts(p, q, params, h, dt):
+    """
+    Calculates the constant parts of the objective function (basically a
+    forwards Euler.)
+    """
     Dp, Dq, C, K = params
     alpha = p.copy()
     beta = q.copy()
@@ -130,6 +147,8 @@ def f_consts(p, q, params, h, dt):
 
 def objective_function(p, q, params, h, dt, consts):
     """
+    Calculates the (negative of the) objective function values for a given
+    input (p and q).
     """
     N = p.shape[0]
     Dp, Dq, C, K = params
@@ -148,17 +167,28 @@ def objective_function(p, q, params, h, dt, consts):
 
 
 def CN_next_step(p, q, params, h, dt, jac_const, maxiter=20, tol=1e-3):
+    """
+    Calculates the next step using the Cranc-Nicholson method. Uses Newtons
+    method to solve the nonlinear set of equations.
+    """
     x = np.array((p, q)).flatten()
     N = p.shape[0]
     consts = f_consts(p, q, params, h, dt)
 
     for k in range(maxiter):
+        # Calculate the new objective function
         f = objective_function(p, q, params, h, dt, consts)
+
+        # Update Jacobian with new inputs
         jac_diag = RD_jacobian_diag(p, q, params, h, dt)
         jac = jac_const + jac_diag
+
+        # Solve, add, propagate
         s = splin.spsolve(jac, f)
         x = x + s
         p, q = x.reshape((2, N, N))
+
+        # Test for convergence
         res = np.sum(np.sqrt(s**2))/s.size
         if res <= tol:
             break
@@ -201,7 +231,7 @@ def main():
     xx, yy = np.meshgrid(x, x)
 
     # Calc timestep
-    dt = h*h/12
+    dt = h*h/(4*max(params)*2)
     Nt = np.ceil(T_end/dt).astype(int)
     t = np.linspace(0, T_end, Nt)
     dt = t[1]-t[0]
@@ -237,8 +267,8 @@ def main():
 
     cmap = 'coolwarm'
     fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(7.5, 4))
-    im1 = ax1.imshow(p_new, cmap=cmap, origin='lower')
-    im2 = ax2.imshow(q_new, cmap=cmap, origin='lower')
+    im1 = ax1.imshow(p_new[1:-1, 1:-1], cmap=cmap, origin='lower')
+    im2 = ax2.imshow(q_new[1:-1, 1:-1], cmap=cmap, origin='lower')
     ax1.set_aspect('equal')
     ax2.set_aspect('equal')
     bounds1 = ax1.get_position().bounds
